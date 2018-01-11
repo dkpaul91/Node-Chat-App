a7fd35b3a5e26a6ffbf423d3e4610e88c9757c36
Dwip Kumar Paul <max.dwip@gmail.com>
2018-01-11 15:44:27 +0530

    Format time stamps using momentjs

--- public/js/index.js
+++ public/js/index.js
@@ -9,9 +9,9 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
-    console.log('New message ', message);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
     
     jQuery('#messages').append(li);
 });
@@ -19,8 +19,9 @@ socket.on('newMessage', function(message) {
 socket.on('newLocationMessage', function (message) {
     var li = jQuery('<li></li>');
     var a = jQuery('<a target="_blank">My Current Location</a>');
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     
-    li.text(`${message.from}: `);
+    li.text(`${message.from} ${formattedTime}: `);
     a.attr('href', message.url);
     li.append(a);
     jQuery('#messages').append(li);
@@ -29,11 +30,13 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function(e) {
     e.preventDefault();
     
+    var messageTextBox = jQuery('[name=message]');
+    
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextBox.val()
     }, function() {
-        
+        messageTextBox.val('');
     });
 });
 
@@ -43,12 +46,17 @@ locationButton.on('click', function () {
         return alert('Location not available on your browser');
     }
     
+    locationButton.attr('disabled', 'disabled').text('Sending Location...');
+    
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send Location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
-    }, function () {
+    }, function (err) {
         alert('Unable to fetch location.');
+        console.log(err);
+        locationButton.removeAttr('disabled').text('Send Location');
     });
 });
