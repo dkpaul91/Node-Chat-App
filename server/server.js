21783dc538265b6700d0d04cdbf83fe9c9588ac2
Dwip Kumar Paul <max.dwip@gmail.com>
2018-01-11 23:09:04 +0530

    Add Users class and updateUserList event

--- server/server.js
+++ server/server.js
@@ -3,21 +3,35 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 8000;
 var app = new express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var users = new Users();
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New User connected');
     
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat App'));
-    
-    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+    socket.on('join', (params, callback) => {
+        if (!isRealString(params.name) || !isRealString(params.room)) {
+            return callback('Name and Room Name is required.');
+        }
+        
+        socket.join(params.room);
+        users.removeUser(socket.id);
+        users.addUser(socket.id, params.name, params.room);
+        
+        io.to(params.room).emit('updateUserList', users.getUserList(params.room));        
+        socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat App'));
+        socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
+        callback();
+    });
     
     socket.on('createMessage', (message, callback) => {
         console.log('Message recieved ', message);
@@ -30,7 +44,12 @@ io.on('connection', (socket) => {
     });
     
     socket.on('disconnect', () => {
-        console.log('Client disconnected');
+        var user = users.removeUser(socket.id);
+        
+        if(user) {
+            io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+            io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
+        }
     });
 });
 
---
+++ server/utils/users.js
@@ -0,0 +1,59 @@
+[{
+    id: '',
+    name: '',
+    room: ''
+}]
+
+class Users {
+    constructor () {
+        this.users = [];
+    }
+    addUser (id, name, room) {
+        var user = {id, name, room};
+        this.users.push(user);
+        return user;
+    }
+    removeUser (id) {
+//        var index;
+//        var user = this.users.filter((user, i) => {
+//            if(user.id === id) {
+//                index = i;                
+//            }
+//            return user.id === id;
+//        });
+//        (index) && this.users.splice(index, 1);
+        var user = this.getUser(id);
+        
+        if (user) {
+            this.users = this.users.filter((user) => user.id !== id);
+        }
+        
+        return user;
+    }
+    getUser (id) {
+        return this.users.filter((user) => user.id === id)[0];
+    }
+    getUserList (room) {
+        var users = this.users.filter((user) => user.room === room);
+        var namesArray = users.map((user) => user.name);
+        
+        return namesArray;
+    }
+}
+
+//class Person {
+//    constructor (name, age) {
+//        this.name = name;
+//        this.age = age;
+//    }
+//    getUserDescription () {
+//        return `${this.name} is ${this.age} year(s) old.`;
+//    }
+//}
+//
+//var me = new Person('Dwip', 26);
+//var description = me.getUserDescription();
+//console.log(description);
+
+
+ module.exports = {Users};
